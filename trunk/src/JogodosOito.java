d2fa34b73aa37e89b02c4b95513bcb8f1f8b35f9
mohammadazul@gmail.com <mohammadazul@gmail.com@f48163ba-fd09-b8dd-b88c-d0f7e54d4caf>
2014-09-11 22:16:31 +0000

    1° Commit (Teste)

---
+++ trunk/src/JogodosOito.java
@@ -0,0 +1,21 @@
+
+Class JogodosOito{
+	int[][] m;
+
+	public int troca(){
+		if(true){
+			return 1;
+		}else{
+			return false;
+		}
+	}
+	
+	public int checaTabuleiro(){
+		if(true){
+			return 1;
+		}else{
+			return false;
+		}
+	}
+
+}	
